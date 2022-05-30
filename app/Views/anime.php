525fef4cf1cd1cc96ae555e45c78ecad841fdadb
Joan Prat Pérez <99180764+joanprat@users.noreply.github.com>
2022-05-30 09:33:24 +0200

    Random2

--- app/Views/anime.php
+++ app/Views/anime.php
@@ -10,6 +10,7 @@
     <!-- Custom -->
     <link rel="stylesheet" href="<?= base_url('assets/css/custom-styles.css') ?>">
     <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
